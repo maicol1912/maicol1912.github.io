058df9a43542b1aa64d5b6e72ab8e36135c7dc19
maicol1912 <maicolarcila34@gmail.com>
2025-01-17 00:30:32 -0500

    Feat: Add initial portfolio

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import robotsTxt from 'astro-robots-txt';
+
+export default defineConfig({
+  integrations: [tailwind(), robotsTxt()],
+  site: 'https://maicol1912.github.io',
+});
